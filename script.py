9cfd9c3f0b031377c0880df87548a484cfc8aac9
Suchster <mr.suchster@gmail.com>
2019-06-14 17:14:28 -0500

    Modified script

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ a = np.linspace(0,10,11)
 
 # print(sys.version)
 print(sys.executable)
-
+print("This program is just a test for module and testing vs code")
 
 def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)

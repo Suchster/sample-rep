c4ac641694d3485635bf66165adb2330a3df1dad
Suchster <mr.suchster@gmail.com>
2019-06-14 14:20:18 -0500

    Initial Commit

---
+++ script.py
@@ -0,0 +1,25 @@
+import math
+import os
+import sys
+import numpy as np
+
+import requests
+
+a = np.linspace(0,10,11)
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+r = requests.get('https://google.com')
+print(r.status_code)
+
+# name = input("What is your name? ") #You must run this through terminal
+
+# print(greet('World'))
+# print(greet(name))
